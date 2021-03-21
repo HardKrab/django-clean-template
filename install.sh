820c19db30383fcc2b8e2731c12a33048696cc10
HardKrab <staskarimov@gmail.com>
2021-03-21 11:39:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ pip install -r requirements.txt
 sed -i "s~dbms_template_path~$project_path~g" nginx/site.conf systemd/gunicorn.service
 sed -i "s~dbms_template_domain~$project_domain~g" nginx/site.conf src/config/settings.py
 
-sudo ln -s $project_path/nginx/site.conf /etc/nginx/sites-enabled/
-sudo ln -s $project_path/systemd/gunicorn.service /etc/systemd/system/
+sudo ln -sf $project_path/nginx/site.conf /etc/nginx/sites-enabled/
+sudo ln -sf $project_path/systemd/gunicorn.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
